810aa0fcb0d4e5a6b4fa02adf5ad29a86ed1db14
Joseph Okonoboh <okonoboh@gmail.com>
2015-03-20 11:49:30 -0700

    hw

---
+++ spring_2015/csulb/cecs_323/a7-okonoboh.sql
@@ -0,0 +1,35 @@
+/*
+ * Joseph Okonoboh
+ * Problem a.
+ */
+SELECT DISTINCT fName, lName, c.street, c.city, c.state, c.zipcode
+FROM customers As c
+INNER JOIN loans
+ON c.id = loans.owner_id
+INNER JOIN branches
+ON loans.branch_id = branches.id
+WHERE branches.state = 'CA'
+UNION
+SELECT DISTINCT fName, lName, c.street, c.city, c.state, c.zipcode
+FROM customers As c
+INNER JOIN depositors
+ON c.id = depositors.cid
+INNER JOIN accounts
+ON depositors.account_number = accounts.account_number
+INNER JOIN branches
+ON accounts.branch_id = branches.id
+WHERE branches.state = 'CA';
+
+/*
+ * Problem b.
+ */
+SELECT fName, lName, street, city, state, zipcode
+FROM customers
+INNER JOIN loans
+ON customers.id = loans.owner_id
+WHERE customers.id NOT IN
+(SELECT customers.id
+FROM customers
+INNER JOIN depositors
+ON customers.id = depositors.cid);
+ 
